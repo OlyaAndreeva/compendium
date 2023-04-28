e59326ebe2bd0f63e143b85295405852b0c07c7b
OlyaAndreeva <shinefhialkova@gmail.com>
2023-04-28 19:20:46 +0300

    new_game

--- games/snake/main.py
+++ games/snake/main.py
@@ -2,19 +2,24 @@ from tkinter import Tk, Canvas
 from snake import Snake
 from config import WIDTH, HEIGHT, SQUARE, center_window
 
+def new_game():
+    my_snake = Snake(window, canvas)
+    my_snake.next_turn()
+
+    window.bind('<Left>', lambda event: my_snake.set_direction('left'))
+    window.bind('<Right>', lambda event: my_snake.set_direction('right'))
+    window.bind('<Up>', lambda event: my_snake.set_direction('up'))
+    window.bind('<Down>', lambda event: my_snake.set_direction('down'))
+    window.bind('<space>', lambda event: my_snake.press_space())
+
+#################################################################################################################################
+
 window = Tk()
 window.title('Snake game')
 canvas = Canvas(window, width=WIDTH*SQUARE, height=HEIGHT*SQUARE, bg='#000000')
 canvas.pack()
+
 center_window(window)
-
-my_snake = Snake(window, canvas)
-my_snake.next_turn()
-
-window.bind('<Left>', lambda event: my_snake.set_direction('left'))
-window.bind('<Right>', lambda event: my_snake.set_direction('right'))
-window.bind('<Up>', lambda event: my_snake.set_direction('up'))
-window.bind('<Down>', lambda event: my_snake.set_direction('down'))
-window.bind('<space>', lambda event: my_snake.press_space())
+new_game()
 
 window.mainloop()
